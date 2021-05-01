6eb031df2ff3f6dfbc4368b593e812c7e0dc6fe8
vijay-tewari <vijaytewari.ml@gmail.com>
2021-05-01 22:49:34 +0530

    echo "# spring5webapplication" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/vijay-tewari/spring5webapplication.git git push -u origin main

---
+++ src/main/java/guru/springFramework/springwebapp/SpringwebappApplication.java
@@ -0,0 +1,13 @@
+package guru.springFramework.springwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebappApplication.class, args);
+	}
+
+}
